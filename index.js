5cfcfd9dc9c3f17381a5279d3c45c3b840cca449
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-03-19 00:56:09 +0300

    i have no idea what's going on rn
    Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ app.use('/', rootLimiter);
 app.use(
 	cors({
 		origin: function (origin, callback) {
-			if (!origin || allowedOrigins.includes(origin)) {
+			if (origin && allowedOrigins.includes(origin)) {
 				callback(null, true);
 			} else {
 				callback(new Error(`Origin ${origin} is not allowed by CORS!`));
+				console.warn(`Connection refused: origin ${origin} is not allowed by CORS!`);
 			}
 		},
 	})
